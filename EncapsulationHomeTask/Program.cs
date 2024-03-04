36182aed61afe8a23429102fefa94920399b9c05
AykhanTa <aykhanft@code.edu.az>
2024-03-05 02:51:48 +0400

    Add project files.

---
+++ EncapsulationHomeTask/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace EncapsulationHomeTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            user.UserName = "Test";
+            user.Password = "";
+            user.Age = -10;
+            
+            
+
+        }
+    }
+}
---
+++ EncapsulationHomeTask/User.cs
@@ -0,0 +1,71 @@
+﻿namespace EncapsulationHomeTask
+{
+    class User
+    {
+        string _userName;
+        public string UserName { get; set; }
+
+        string _password;
+        public string Password {
+            get 
+            { 
+                return _password;
+            }
+            set 
+            {
+                if (string.IsNullOrEmpty(value)==true || value.Length<8)
+                {
+                    Console.WriteLine("Duzgun formatda daxil edilmeyib.");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine(RulesOfPassword(value));
+                }
+            }
+        }
+
+        int _age;
+        public int Age { 
+            get 
+            {
+                return _age;
+            }
+            set 
+            { 
+                if (value<0)
+                {
+                    Console.WriteLine("Yash menfi ola bilmez.");
+                    return;
+                }
+                _age = value;
+            }
+        }
+
+        private string RulesOfPassword(string password)
+        {
+            int count = 0;
+            int count1=0;
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (char.IsUpper(password[i]))
+                {
+                    count++;
+                }
+                else if (char.IsDigit(password[i]))
+                {
+                    count1++;
+                }
+            }
+            if (count>0 && count1>0)
+            {
+                return password;
+            }
+            else
+            {
+                return "Uncorrect password format";
+            }
+
+        }
+    }
+}
